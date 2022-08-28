2aa9783694185940f3091e3629ea0f92c88b7924
Masaki Koyanagi <mascii@gmail.com>
2022-08-28 11:57:53 +0900

    add components for Form

---
+++ components/ErrorMessage.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+defineProps({
+  error: {
+    type: Error,
+    required: false,
+  },
+});
+</script>
+
+<template>
+  <span v-if="error" style="display: inline-flex; align-items: center"
+    ><carbon-warning style="flex-shrink: 0" />{{ error.message }}</span
+  >
+</template>
---
+++ components/FieldSet.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const touched = ref(false);
+
+const onFocusoutHandler = () => {
+  touched.value = true;
+};
+</script>
+
+<template>
+  <fieldset @focusout="onFocusoutHandler">
+    <slot :touched="touched" />
+  </fieldset>
+</template>
